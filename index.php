854f9188ac270d5b272e5fa555313b70cf0a78f7
Piyush Kumar <142146271+PiyushChoudhry@users.noreply.github.com>
2024-07-10 23:00:59 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+    $insert = false;
+    if(isset($_POST['name'])){
+        $server = "localhost";
+        $username = "root";
+        $password = "";
+
+        $con = mysqli_connect($server, $username, $password); // con = connect
+        if (!$con) {
+            die("Connection failed: ".mysqli_connect_error());
+        }
+        // echo "successfully connected to DB";
+
+        $name = $_POST['name'];
+        $age = $_POST['age'];
+        $gender = $_POST['gender'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $desc = $_POST['desc'];
+        $sql = "INSERT INTO `trip`.`trip` (`NAME`, `AGE`, `GENDER`, `EMAIL`, `PHONE`, `DESC`, `DT`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
+        // echo $sql;
+
+        if($con->query($sql)==true){
+            // echo "Successfully inserted";
+            $insert = true;
+        }
+        else{
+            echo "Error: $sql <br> $con->error";
+        }
+        $con->close();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Travel Form</title>
+    <link href="https://fonts.googleapis.com/css2?family=Edu+NSW+ACT+Foundation:wght@400..700&family=Libre+Baskerville&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&family=Edu+NSW+ACT+Foundation:wght@400..700&family=Libre+Baskerville&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+    <img class="bg" src="DTU.JPG" alt="DTU">
+    <div class="container">
+        <h3>Welcome to DTU - US Trip Form</h3>
+        <p>Enter your details and submit your form to confrim your participation in a trip</p>
+        <?php
+        if($insert == true){
+            echo "<p class='submitMSG'>Thanks for submitting your form. We are happy to see you joining us for the US trip.</p>";
+        }
+        ?>
+        <form action="index.php" method="post">
+            <input type="text" name="name" id="name"  placeholder="Enter your name">
+            <input type="text" name="age" id="age"  placeholder="Enter your age">
+            <input type="text" name="gender"nder id="gender"  placeholder="Enter your gender">
+            <input type="email" name="email" id="email" placeholder="Enter your email address">
+            <input type="phone" name="phone" id="phone" placeholder="Enter your phone">
+            <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Enter any other information here"></textarea>
+            <button class="btn">Submit</button>
+            <button class="btn">Reset</button>
+        </form>
+    </div>
+    <script src="index.js"></script>
+    <!-- INSERT INTO `trip` (`SNO`, `NAME`, `AGE`, `GENDER`, `EMAIL`, `PHONE`, `OTHER`, `DT`) VALUES ('1', 'textName', '23', 'Male', 'abc@gmail.com', '0123456789', 'This is my first ever php myadmin msg.', current_timestamp()); -->
+</body>
+</html>
